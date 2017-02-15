0750812b6db9504d7fdcbab0caa6b84d1c1f0fb5
AndréLuis <almmarmo@gmail.com>
2017-02-15 19:13:25 -0200

    keys file

---
+++ keys.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+    consumer_key: 'OTS0lxU6U48cgGH4jOHqSpJYY',
+    consumer_secret: '2tBoW00WObEhwwcoQoF1BDaa65kV2IrR4P2msaCWGRvOswjZz7',
+    access_token: '10474752-Oyh1oBbWrCwP04numA0qZTqFFOkBzOYJpF4m7Pbah',
+    access_token_secret: 'Fm1LloLW2xyu2xYPSzQua1gfurU5cpVyHJdYOaQ7CZcUX'
+};
