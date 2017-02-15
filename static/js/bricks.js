fca2b5e0e442555527cd919f76f74a08b046d22d
AndréLuis <almmarmo@gmail.com>
2017-02-15 19:09:32 -0200

    twitter bricks

---
+++ static/js/bricks.js
@@ -0,0 +1,34 @@
+﻿var app = angular.module('twitterBricks', []);
+
+app.controller("BricksController", function ($scope) {
+    $scope.hashTag = "";
+    $scope.buttonStartStopText = "Start";
+    var url = "http://nodejs-project122.44fs.preview.openshiftapps.com/";
+    var socket = io.connect(url);
+
+    function init() {
+        socket.on("stream", function (tweet) {
+            $("#thewall").prepend('<li class="tweetli"><a href="http://www.twitter.com/'+tweet.username+'" target="_blank"><img src="' + tweet.icon + '" alt="" /></a><div class="name">' + tweet.name + ' (@' + tweet.username + ')</div><div class="message">' + tweet.text + '</div></li>');
+            //$(".list").html(tweet.hash);
+
+            var colors = ["#3F602B", "#77896C", "#008B8B", "#528B8B", "#567E3A", "#55AE3A", "#458B74", "#174038", "#20BF9F", "#01C5BB", "#457371", "#78AB46", "#FF4040", "#EE5C42", "#CD3700", "#29242", "#B87333", "#8B795E"];
+            var rand = Math.floor(Math.random() * colors.length);
+            $("li:first-child").css("background-color", colors[rand]);
+        });
+    }
+
+    $scope.clickStartStop = function () {
+        if ($scope.buttonStartStopText == "Start") {
+            socket.emit("hash", {
+                hash: $scope.hashTag
+            });
+            $scope.buttonStartStopText = "Stop";
+        }
+        else {
+            socket.emit("stoptwit");
+            $scope.buttonStartStopText = "Start";
+        }
+    }
+
+    init();
+});
