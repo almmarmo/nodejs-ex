b8e9a2141a5111f3e8b3c4a7c5ae7cc177ba74dd
AndréLuis <almmarmo@gmail.com>
2017-02-16 15:06:38 -0200

    sensitive

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 ﻿module.exports = {
-    consumer_key: '',
-    consumer_secret: '',
+    consumer_key: 'OTS0lxU6U48cgGH4jOHqSpJYY',
+    consumer_secret: '2tBoW00WObEhwwcoQoF1BDaa65kV2IrR4P2msaCWGRvOswjZz7',
     access_token: '',
     access_token_secret: ''
 };
